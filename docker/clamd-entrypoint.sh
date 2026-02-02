4380dc058442b073e1d5147569db82dbf2286d5e
Tommaso Rossi <tommaso.rossi@pagopa.it>
2026-02-02 15:41:59 +0100

    feat(clamd): Allow explicit schedule of db refreshes (#11)

--- docker/clamd-entrypoint.sh
+++ docker/clamd-entrypoint.sh
@@ -6,13 +6,21 @@
 set -e
 
 # redirect daemons output to stderr
-tail --pid 1 -n0 -F /var/log/clamav/clamd.log /var/log/clamav/freshclam.log > /dev/stderr &
+clamd_log=/var/log/clamav/clamd.log
+freshclam_log=/var/log/clamav/freshclam.log
+touch $clamd_log $freshclam_log
+tail --pid 1 -n0 -F $clamd_log $freshclam_log > /dev/stderr &
 
 # update antivirus database
 freshclam
 # start the clamav daemon
 clamd
-# start the freshclam daemon to keep automated updates
-freshclam -d
+
+# start supercronic for scheduled freshclam updates. we don't start
+# freshclam -d because you can't configure the timing correctly, you
+# can just specify how many times a day. we want a real cron here.
+echo "${CLAMAV_DB_REFRESH_CRON} /usr/bin/freshclam >> $freshclam_log 2>&1" > /tmp/freshclam-cron
+supercronic /tmp/freshclam-cron &
 
 exec "$@"
+
