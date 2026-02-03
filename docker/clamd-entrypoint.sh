3d8eb64e276bbf5b69fa5f10992aa6c982a49c30
Tommaso Rossi <tommaso.rossi@pagopa.it>
2026-02-03 14:06:18 +0100

    feat(clamd): Testing mode for clamd bundled image (#12)

--- docker/clamd-entrypoint.sh
+++ docker/clamd-entrypoint.sh
@@ -11,16 +11,25 @@ freshclam_log=/var/log/clamav/freshclam.log
 touch $clamd_log $freshclam_log
 tail --pid 1 -n0 -F $clamd_log $freshclam_log > /dev/stderr &
 
-# update antivirus database
-freshclam
+# handle test mode vs production database
+if [ "$CLAMAV_TEST_MODE" = "true" ]; then
+    # create minimal eicar-only database.  this will greatly reduce
+    # memory usage in testing environments but will just recognize the
+    # EICAR test file as virus
+    echo "44d88612fea8a8f36de82e1278abb02f:68:EICAR-Test-Signature" > /var/lib/clamav/test.hdb
+    echo "WARNING: Running in TEST MODE. Minimal signatures loaded, DO NOT USE IN PRODUCTION"
+else
+    # update antivirus database
+    freshclam
+
+    # start supercronic for scheduled freshclam updates. we don't start
+    # freshclam -d because you can't configure the timing correctly, you
+    # can just specify how many times a day. we want a real cron here.
+    echo "${CLAMAV_DB_REFRESH_CRON} /usr/bin/freshclam >> $freshclam_log 2>&1" > /tmp/freshclam-cron
+    supercronic /tmp/freshclam-cron &
+fi
+
 # start the clamav daemon
 clamd
 
-# start supercronic for scheduled freshclam updates. we don't start
-# freshclam -d because you can't configure the timing correctly, you
-# can just specify how many times a day. we want a real cron here.
-echo "${CLAMAV_DB_REFRESH_CRON} /usr/bin/freshclam >> $freshclam_log 2>&1" > /tmp/freshclam-cron
-supercronic /tmp/freshclam-cron &
-
 exec "$@"
-
